f71b2966ea909984bdef68074d8e454a7a384c85
HYENA-007-CYBER <hyensteen10@gmail.com>
2025-04-05 20:43:19 +0530

    Update medium_4th.py

--- medium_4th.py
+++ medium_4th.py
@@ -1,21 +1,21 @@
-file_path = "log.txt"  # Replace with actual file path
+file_path = "log.txt"  # Replace with desired file path
 
 with open(file_path, "r") as file:
     data = list(map(int, file.readline().strip().split(",")))
 print(data)
 
 
-size= 3
-import numpy as np
+size= 3 # declaring globally (Can easily change here)
+import numpy as np#for standard deviation
 data = [4, 7, 6, 1, 8]
-def muchiko_filter(data, size):
+def muchiko_filter(data, size):# getting average
     muchiko = []  
     for i in range(len(data) - size + 1):
         muchiko.append(sum(data[i:i+size]) // size) 
     return muchiko
 m = muchiko_filter(data,size)
 print("muchiko_result : ",m)  
-def sanchiko_filter(data, size):
+def sanchiko_filter(data, size):# getting median
     sanchiko = []  
     for i in range(len(data) - size + 1):
         sanchiko.append(sorted(data[i:i+size])[size//2])
@@ -26,17 +26,19 @@ hybrid_result_1 = sanchiko_filter(m,size)
 print("Hybrid Filtered Data (Muchiko then Sanchiko) : ", hybrid_result_1)
 hybrid_result_2 = muchiko_filter(s,size)
 print("Hybrid Filtered Data (Sanchiko then Muchiko) : ", hybrid_result_2)
-std_original=np.std(data)
+std_original=np.std(data)#USING STANDARD DEVIATION FROM NUMPY
 std_m=np.std(m)
 std_s=np.std(s)
 std_h1=np.std(hybrid_result_1)
 std_h2=np.std(hybrid_result_2)
+#TO CALCULATE BY HOW MUCH PERCENT IT FILTERS THE DATA
 def per_red(x):
     return ((std_original-x)/std_original)*100
 m_per=per_red(std_m)
 s_per=per_red(std_s)
 h1_per=per_red(std_h1)
 h2_per=per_red(std_h2)
+#TO PRINT WHICH FILTER IS BEST FOR THE DATASET
 if ((m_per>=s_per)and(m_per>=h1_per)and(m_per>=h2_per)):
     print("MUCHIKO FILTER IS BEST")
 if ((s_per>=m_per)and(s_per>=h1_per)and(s_per>=h2_per)):
